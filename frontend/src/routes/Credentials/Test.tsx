a5606ae2c43901cbfe99aa2127f658d180c44360
Kevin Cormier <kcormier@redhat.com>
2021-11-08 16:36:15 -0500

    Update to 0.0.3 dynamic-plugin-sdk
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/routes/Credentials/Test.tsx
+++ frontend/src/routes/Credentials/Test.tsx
@@ -1,16 +1,12 @@
 /* Copyright Contributors to the Open Cluster Management project */
-import { Page, PageSection } from '@patternfly/react-core'
+import { Page, PageSection, Title } from '@patternfly/react-core'
 
 export default function Test() {
     return (
-        <Page>
-            <PageSection variant="light">{/* <Title headingLevel="h5">Test page</Title> */}</PageSection>
+        <Page additionalGroupedContent={<Title headingLevel="h4">Page title</Title>} groupProps={{ sticky: 'top' }}>
+            <PageSection variant="light">
+                <Title headingLevel="h5">Test page</Title>
+            </PageSection>
         </Page>
-        // Below doees not work...
-        // <Page additionalGroupedContent={<Title headingLevel="h4">Page title</Title>} groupProps={{ sticky: 'top' }}>
-        //     <PageSection variant="light">
-        //         <Title headingLevel="h5">Test page</Title>
-        //     </PageSection>
-        // </Page>
     )
 }
