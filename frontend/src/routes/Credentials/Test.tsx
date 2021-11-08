64a18a6d8d57f2d0510e3b6155f25cb365df2843
James Talton <jtalton@redhat.com>
2021-11-08 13:41:08 -0500

    simplify
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/src/routes/Credentials/Test.tsx
+++ frontend/src/routes/Credentials/Test.tsx
@@ -1,5 +1,18 @@
 /* Copyright Contributors to the Open Cluster Management project */
+import { Page, PageSection, Title } from '@patternfly/react-core'
 
 export default function Test() {
-    return <div>Test</div>
+    return (
+        <Page>
+            <PageSection variant="light">
+                <Title headingLevel="h5">Test page</Title>
+            </PageSection>
+        </Page>
+        // Below doees not work...
+        // <Page additionalGroupedContent={<Title headingLevel="h4">Page title</Title>} groupProps={{ sticky: 'top' }}>
+        //     <PageSection variant="light">
+        //         <Title headingLevel="h5">Test page</Title>
+        //     </PageSection>
+        // </Page>
+    )
 }
