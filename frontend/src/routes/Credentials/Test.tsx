b0bbdd5594f60e1a761a666ba7a358201b67879c
James Talton <jtalton@redhat.com>
2021-11-08 13:41:23 -0500

    working
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/src/routes/Credentials/Test.tsx
+++ frontend/src/routes/Credentials/Test.tsx
@@ -1,12 +1,10 @@
 /* Copyright Contributors to the Open Cluster Management project */
-import { Page, PageSection, Title } from '@patternfly/react-core'
+import { Page, PageSection } from '@patternfly/react-core'
 
 export default function Test() {
     return (
         <Page>
-            <PageSection variant="light">
-                <Title headingLevel="h5">Test page</Title>
-            </PageSection>
+            <PageSection variant="light">{/* <Title headingLevel="h5">Test page</Title> */}</PageSection>
         </Page>
         // Below doees not work...
         // <Page additionalGroupedContent={<Title headingLevel="h4">Page title</Title>} groupProps={{ sticky: 'top' }}>
